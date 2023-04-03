b787739854d4b7086ea3e240e6dba83a2dc65277
Daniel Gonzalez <dagon@bu.edu>
2023-04-03 19:25:14 -0400

    Created frontend skeleton

---
+++ dev/script.js
@@ -0,0 +1,16 @@
+let swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    //spaceBetween: 20,
+    loopFillGroupWithBlank: true,
+    //loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+});
