3b94ade0539fa3c9d03ca8e805895b1fcce5a8af
EthRousseau <erousseau@ptc.com>
2023-05-09 15:53:44 -0400

    Move to prod safe server

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,6 +1,7 @@
 from website import create_app
-
+from waitress import serve
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    #app.run(host="0.0.0.0", port=8080, debug=True)
+    serve(app, host="0.0.0.0", port=8080)
